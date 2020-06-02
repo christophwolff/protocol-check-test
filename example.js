eab803264a3319ad8347ed862bdd8cfda384047b
Christoph Wolff <cw@christophwolff.de>
2020-06-02 16:11:53 +0200

    add click

--- example.js
+++ example.js
@@ -4,4 +4,10 @@ $(function () {
       $(this).css('background-color', 'red')
     }.bind(this))
   })
+  $('button[href]').click(function (event) {
+    window.protocolCheck($(this).attr('href'), function () {
+      alert('protocol not recognized')
+    })
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false)
+  })
 })
