1f85e8c7df0822fe28b70cd975710637cb450ea0
Christoph Wolff <cw@christophwolff.de>
2020-06-02 16:22:36 +0200

    Add Success Func

--- example.js
+++ example.js
@@ -3,13 +3,17 @@ $(function () {
     setTimeout(() => {
       window.protocolCheck($(this).attr('href'), function () {
         $(this).css('background-color', 'red')
+      }.bind(this), function () {
+        $(this).css('background-color', 'red')
       }.bind(this))
     }, 400);
   })
   $('button[href]').click(function (event) {
     window.protocolCheck($(this).attr('href'), function () {
       alert('protocol not recognized')
-    })
+    }, function () {
+      alert('protocol recognized')
+    }.bind(this))
     event.preventDefault ? event.preventDefault() : (event.returnValue = false)
   })
 })
