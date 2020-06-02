c465c987e0307c0cc491fde3c4cb8d44fb4a52d8
Christoph Wolff <cw@christophwolff.de>
2020-06-02 14:10:15 +0200

    Init

---
+++ example.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('div[href]').click(function (event) {
+    window.protocolCheck($(this).attr('href'), function () {
+      alert('protocol not recognized')
+    })
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false)
+  })
+})
